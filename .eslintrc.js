4a2f4f07f21a778555192e895e4344d1cc59ea10
KalininaOI <kalininanana@gmail.com>
2022-06-15 23:16:42 +0300

    folders

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest', 'import'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'linebreak-style': 'off',
+    'react/function-component-definition': 'off',
+    'default-param-last': 'off',
+'@typescript-eslint/default-param-last': 'off',
+ 'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
