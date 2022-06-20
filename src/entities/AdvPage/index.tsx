782d7fb1fa28569383177a5fc16f9dcf9c09d7e6
KalininaOI <kalininanana@gmail.com>
2022-06-20 23:13:00 +0300

    advPageHeader

--- src/entities/AdvPage/index.tsx
+++ src/entities/AdvPage/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import style from './AdvPage.module.scss';
 
 const AdvPage = () => {
-  return <div>Goods</div>;
+  return (
+    <div>
+      <div className={style.advpage_header}>
+        <div>
+          <h1>Объявления</h1>
+          <p>Всего: counter </p>
+        </div>
+        <button type="button">Добавить +</button>
+      </div>
+      <div>Search and ragination</div>
+      <div>Tab</div>
+    </div>
+  );
 };
 
 export default AdvPage;
