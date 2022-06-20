c991664fc6b2db49d0bde5dc01a582f0be5de7cf
KalininaOI <kalininanana@gmail.com>
2022-06-20 20:32:59 +0300

    SideBar

--- src/shared/Sidebar/index.tsx
+++ src/shared/Sidebar/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import Imges from '../Imges/Imges';
+// import { Link, useLocation } from 'react-router-dom';
 // import { sidebarConfig } from './sidebarConfig';
-// import style from './Sidebar.module.scss';
+import style from './Sidebar.module.scss';
 
 const Sidebar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className={style.sidebar_menu}>
+      <div className={`${style.sidebar_menu__acc} ${style.sidebar_menu__block}`}>
+        <div className={style.sidebar_menu__acc_alogo}>
+          {' '}
+          <Imges imgName="ALogo" />
+        </div>
+        <div>
+          <ul className={style.sidebar_menu__acc_adm}>
+            {' '}
+            <li className={style.sidebar_menu__acc_adm_name}>Super admin</li>
+            <li className={style.sidebar_menu__acc_adm_desc}>Админ-меню</li>
+          </ul>
+        </div>
+      </div>
+      <div className={`${style.sidebar_menu__pages} ${style.sidebar_menu__block}`}>
+        {' '}
+        <Imges imgName="AdvLogo" />
+      </div>
+      <div className={`${style.sidebar_menu__exit} ${style.sidebar_menu__block}`}>
+        {' '}
+        <Imges imgName="ExitLogo" />
+      </div>
+    </div>
+  );
   // const { pathname } = useLocation();
 
   // return (
