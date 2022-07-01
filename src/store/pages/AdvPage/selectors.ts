82051e57c9f6de1a5a3a5b29cd3524cbb19c42c5
KalininaOI <kalininanana@gmail.com>
2022-07-01 17:37:53 +0300

    store_error2

--- src/store/pages/AdvPage/selectors.ts
+++ src/store/pages/AdvPage/selectors.ts
@@ -1,6 +1,5 @@
 import { IStore } from '../../i';
 
 export const getItemsData = (state: IStore) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
   state.items.itemsData;
 };
